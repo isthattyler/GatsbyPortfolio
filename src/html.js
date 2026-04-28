d3cdea221a5b43f4e0a94f6e4627f537c4527525
Tyler Nguyen <isthattyler@Tylers-MacBook-Pro-13.local>
2026-04-27 23:16:52 -0400

    refactor: add Google Fonts and dark-first CSS variables foundation

--- src/html.js
+++ src/html.js
@@ -19,6 +19,12 @@ export default function HTML({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&family=Manrope:wght@400;500;600;700&family=Playfair+Display:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
         {headComponents}
       </head>
       <body {...bodyAttributes}>
